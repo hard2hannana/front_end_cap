66202486db6b3b3c44162aa66264bda336e4777c
hard2hannana <hard2hannana>
2025-07-14 13:06:11 -0400

    almost complete layout

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,68 +1,73 @@
 import React from "react";
+import reviewer from "../assets/mid60female.png";
+
+
 
 export default function Reviews() {
   return (
     <section className="reviews">
-      <div className="container container-body">
-      <h2 className="reviews-title">Our customers love us!</h2>
-      <div className="cards">
-        <div className="card-item">
-          <div className="card-footer">
-            <img src="/src/assets/mid60female.png" alt="" />
-              <div className="reviewer-details">
-                <h4>Reviewers name</h4>
-                <small className="review-date">Review date"</small>
-              </div>
-          </div>
-          <div className="card-content">
-            <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
-            <p>Review title</p>
-            <p>Review body</p>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="card-content">
-            <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
-            <p>Review title</p>
-            <p>Review body</p>
+      <div className="container">
+      <div className="reviews">
+        <h2 className="reviews-title">Our customers love us!</h2>
+        <div className="cards spacing">
+          <div className="card-item">
             <div className="card-footer flex-center">
-              <img src="/src/assets/mid60female.png" alt="" />
-              <div className="reviewer-details">
-                <h4>Reviewers name</h4>
-                <small className="review-date">Review date"</small>
-              </div>
+              <img src={reviewer} alt="Mid sixties female" />
+                <div className="reviewer-details">
+                  <h4>Reviewers name</h4>
+                  <small className="review-date">Review date"</small>
+                </div>
+            </div>
+            <div className="card-content">
+              <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
+              <p>Review title</p>
+              <p>Review body</p>
+            </div>
+          </div>
+          <div className="card-item">
+            <div className="card-footer flex-center">
+              <img src={reviewer} alt="Mid sixties female" />
+                <div className="reviewer-details">
+                  <h4>Reviewers name</h4>
+                  <small className="review-date">Review date"</small>
+                </div>
+            </div>
+            <div className="card-content">
+              <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
+              <p>Review title</p>
+              <p>Review body</p>
+            </div>
+          </div>
+          <div className="card-item">
+            <div className="card-footer flex-center">
+              <img src={reviewer} alt="Mid sixties female" />
+                <div className="reviewer-details">
+                  <h4>Reviewers name</h4>
+                  <small className="review-date">Review date"</small>
+                </div>
+            </div>
+            <div className="card-content">
+              <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
+              <p>Review title</p>
+              <p>Review body</p>
+            </div>
+          </div>
+          <div className="card-item">
+            <div className="card-footer flex-center">
+              <img src={reviewer} alt="Mid sixties female" />
+                <div className="reviewer-details">
+                  <h4>Reviewers name</h4>
+                  <small className="review-date">Review date"</small>
+                </div>
+            </div>
+            <div className="card-content">
+              <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
+              <p>Review title</p>
+              <p>Review body</p>
             </div>
           </div>
         </div>
-        <div className="card-item">
-          <div className="card-content">
-            <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
-            <p>Review title</p>
-            <p>Review body</p>
-            <div className="card-footer flex-center">
-              <img src="/src/assets/mid60female.png" alt="" />
-              <div className="reviewer-details">
-                <h4>Reviewers name</h4>
-                <small className="review-date">Review date"</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card-item">
-          <div className="card-content">
-            <h3 className="stars">⭐ ⭐ ⭐ ⭐ ⭐</h3>
-            <p>Review title</p>
-            <p>Review body</p>
-            <div className="card-footer flex-center">
-              <img src="/src/assets/mid60female.png" alt="" />
-              <div className="reviewer-details">
-                <h4>Reviewers name</h4>
-                <small className="review-date">Review date"</small>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>  
+      </div>
     </div>
     </section>
   ); 
--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -1,54 +1,65 @@
 import React from "react";
+import car from "../assets/car.png";
+import greekSalad from "../assets/greekSalad.jpg";
+import bruchetta from "../assets/bruchetta.svg";
+import lemonDessert from "../assets/lemonDessert.jpg";
+
 
 export default function Specials() {
   return (
     <section className="specials ">
-      <div className="container container-body">
-        <h2>This week's specials!</h2>
-        <button>Order Online</button>
-        <div className="cards">
-          <div className="card-item">
-            <img src="/src/assets/greekSalad.jpg" alt="Special 1" />
-            <div className="card-content">
-              <div className="flex">
-                <h3>Greek Salad</h3>
-                <h4>$12.99</h4>
-              </div>    
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-              <div className="card-footer">
-                <h3>Order Delivery</h3>
-                <img src="/src/assets/car.png" alt="" />
-              </div>  
-            </div>
+      <div className="container">
+        <div className="specials">
+          <div className="specials-title">
+            <h2>This week's specials!</h2>
+            <button>Order Online</button>
           </div>
-          <div className="card-item">
-            <img src="/src/assets/greekSalad.jpg" alt="Special 1" />
-            <div className="card-content">
-              <div className="flex">
-                <h3>Greek Salad</h3>
-                <h4>$12.99</h4>
-              </div>    
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-              <div className="card-footer">
-                <h3>Order Delivery</h3>
-                <img src="/src/assets/car.png" alt="" />
-              </div>  
+            <div className="specials-body">
+              <div className="cards">
+                <div className="card-item">
+                  <img src={greekSalad} alt="Greek Salad" />
+                  <div className="card-content">
+                    <div className="flex">
+                      <h3>Greek Salad</h3>
+                      <h4>$12.99</h4>
+                    </div>
+                    <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                    <div className="card-footer">
+                      <h3>Order Delivery</h3>
+                      <img src={car} alt="small car icon" />
+                    </div>
+                  </div>
+                </div>
+                <div className="card-item">
+                  <img src={bruchetta} alt="Special 1" />
+                  <div className="card-content">
+                    <div className="flex">
+                      <h3>Greek Salad</h3>
+                      <h4>$12.99</h4>
+                    </div>
+                    <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                    <div className="card-footer">
+                      <h3>Order Delivery</h3>
+                      <img src={car} alt="small car icon" />
+                    </div>
+                  </div>
+                </div>
+                <div className="card-item">
+                  <img src={lemonDessert} alt="Special 1" />
+                  <div className="card-content">
+                    <div className="flex">
+                      <h3>Greek Salad</h3>
+                      <h4>$12.99</h4>
+                    </div>
+                    <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+                    <div className="card-footer">
+                      <h3>Order Delivery</h3>
+                      <img src={car} alt="small car icon" />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="card-item">
-            <img src="/src/assets/greekSalad.jpg" alt="Special 1" />
-            <div className="card-content">
-              <div className="flex">
-                <h3>Greek Salad</h3>
-                <h4>$12.99</h4>
-              </div>        
-              <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-              <div className="card-footer">
-                <h3>Order Delivery</h3>
-                <img src="/src/assets/car.png" alt="" />
-              </div>  
-            </div>
-          </div>
         </div>
       </div>
     </section>
